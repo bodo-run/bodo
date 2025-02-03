5585f6eed6903a967aa031ee73ffa0f87172c3c2
Mohsen Azimi <me@azimi.me>
2025-02-03 09:58:19 +0700

    allow ollama too

--- src/plugins/mod.rs
+++ src/plugins/mod.rs
@@ -3,7 +3,6 @@ pub use crate::plugin::{Plugin, PluginConfig, PluginManager};
 pub mod concurrent_plugin;
 pub mod env_plugin;
 pub mod execution_plugin;
-pub mod failing_plugin;
 pub mod path_plugin;
 pub mod prefix_plugin;
 pub mod print_list_plugin;
